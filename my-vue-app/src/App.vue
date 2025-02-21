0e87a498dcc33366f791b7aa38577ac9de1263db
01101003 <rads7219@gmail.com>
2025-02-22 00:13:11 +0200

    modified:   my-vue-app/src/App.vue

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,100 +1,78 @@
 <template>
   <Analytics>
     <div id="app">
-    <Sidebar 
-      :isLoggedIn="isLoggedIn"
-      :username="username"
-      :userPlan="userPlan"
-      @show-about="showAboutUs = true; showOverlay = true"
-      @show-login="showAuthForm($event)"
-    />
-    
-    <div class="main-content" :class="{ 'with-sidebar': true }">
-      <router-view></router-view>
-    </div>
-    
-    <!-- Overlay and modals -->
-    <div class="overlay" v-show="showOverlay" @click="closeModals"></div>
-    
-    <!-- About Us Container -->
-    <div class="about-us-container" v-show="showAboutUs" @click.stop>
-      <h2>ABOUT PREMIUM CALCULATOR</h2>
+      <Sidebar 
+        :isLoggedIn="isLoggedIn"
+        :username="username"
+        :userPlan="userPlan"
+        @show-about="showAboutUs = true; showOverlay = true"
+        @show-login="showAuthForm($event)"
+        @logout="handleLogout"
+      />
       
-      <div class="mission-statement">
-        <p>We are passionate mathematicians and computer scientists dedicated to making complex mathematical computations accessible, fun, and intuitive. Our mission is to transform intimidating mathematical challenges into engaging, pixel-perfect experiences.</p>
+      <div class="main-content" :class="{ 'with-sidebar': true }">
+        <router-view></router-view>
       </div>
-  
-      <div class="team-section">
-        <div class="team-member">
-          <img src="" alt="Team Member 1">
-          <h3>Ryan. Iv</h3>
-          <p>Founder & Lead Developer</p>
-        </div>
-        <div class="team-member">
-          <img src="" alt="Team Member 2">
-          <h3>Sofia. </h3>
-          <p>Designer</p>
-        </div>
-      </div>
-    </div>
-    
-    <!-- Login Form -->
-    <form class="form" v-show="showLoginForm" @click.stop @submit.prevent="submitAuth">
-      <div class="title">Welcome,<br><span>{{ isSignUp ? 'sign up to join us' : 'sign in to continue' }}</span></div>
-      <input type="email" placeholder="Email" v-model="email" class="input" />
-      <input type="password" placeholder="Password" v-model="password" class="input" />
-      <input v-if="isSignUp" type="password" placeholder="Confirm Password" v-model="confirmPassword" class="input" />
       
-      <div class="login-with">
-        <div class="button-log"><b>t</b></div>
-        <div class="button-log">
-          <svg class="icon" height="56.6934px" id="Layer_1" style="enable-background:new 0 0 56.6934 56.6934;" version="1.1" viewBox="0 0 56.6934 56.6934" width="56.6934px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <path d="M51.981,24.4812c-7.7173-0.0038-15.4346-0.0019-23.1518-0.001c0.001,3.2009-0.0038,6.4018,0.0019,9.6017  c4.4693-0.001,8.9386-0.0019,13.407,0c-0.5179,3.0673-2.3408,5.8723-4.9258,7.5991c-1.625,1.0926-3.492,1.8018-5.4168,2.139  c-1.9372,0.3306-3.9389,0.3729-5.8713-0.0183c-1.9651-0.3921-3.8409-1.2108-5.4773-2.3649  c-2.6166-1.8383-4.6135-4.5279-5.6388-7.5549c-1.0484-3.0788-1.0561-6.5046,0.0048-9.5805  c0.7361-2.1679,1.9613-4.1705,3.5708-5.8002c1.9853-2.0324,4.5664-3.4853,7.3473-4.0811c2.3812-0.5083,4.8921-0.4113,7.2234,0.294  c1.9815,0.6016,3.8082,1.6874,5.3044,3.1163c1.5125-1.5039,3.0173-3.0164,4.527-4.5231c0.7918-0.811,1.624-1.5865,2.3908-2.4196  c-2.2928-2.1218-4.9805-3.8274-7.9172-4.9056C32.0723,4.0363,26.1097,3.995,20.7871,5.8372  C14.7889,7.8907,9.6815,12.3763,6.8497,18.0459c-0.9859,1.9536-1.7057,4.0388-2.1381,6.1836  C3.6238,29.5732,4.382,35.2707,6.8468,40.1378c1.6019,3.1768,3.8985,6.001,6.6843,8.215c2.6282,2.0958,5.6916,3.6439,8.9396,4.5078  c4.0984,1.0993,8.461,1.0743,12.5864,0.1355c3.7284-0.8581,7.256-2.6397,10.0725-5.24c2.977-2.7358,5.1006-6.3403,6.2249-10.2138  C52.5807,33.3171,52.7498,28.8064,51.981,24.4812z"></path>
-          </svg>
+      <!-- Overlay and modals -->
+      <div class="overlay" v-show="showOverlay" @click="closeModals"></div>
+      
+      <!-- About Us Container -->
+      <div class="about-us-container" v-show="showAboutUs" @click.stop>
+        <h2>ABOUT PREMIUM CALCULATOR</h2>
+        
+        <div class="mission-statement">
+          <p>We are passionate mathematicians and computer scientists dedicated to making complex mathematical computations accessible, fun, and intuitive. Our mission is to transform intimidating mathematical challenges into engaging, pixel-perfect experiences.</p>
         </div>
-        <div class="button-log">
-          <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="56.693px" viewBox="0 0 56.693 56.693" version="1.1" id="Layer_1" height="56.693px" class="icon">
-            <path d="M40.43,21.739h-7.645v-5.014c0-1.883,1.248-2.322,2.127-2.322c0.877,0,5.395,0,5.395,0V6.125l-7.43-0.029  c-8.248,0-10.125,6.174-10.125,10.125v5.518h-4.77v8.53h4.77c0,10.947,0,24.137,0,24.137h10.033c0,0,0-13.32,0-24.137h6.77  L40.43,21.739z"></path>
-          </svg>
+    
+        <div class="team-section">
+          <div class="team-member">
+            <img src="" alt="Team Member 1">
+            <h3>Ryan. Iv</h3>
+            <p>Founder & Lead Developer</p>
+          </div>
+          <div class="team-member">
+            <img src="" alt="Team Member 2">
+            <h3>Sofia. </h3>
+            <p>Designer</p>
+          </div>
         </div>
       </div>
-      <button class="button-confirm" type="submit">{{ isSignUp ? 'Sign Up →' : 'Let`s go →' }}</button>
-    </form>
-  </div>
-  <div>
-    
-    <p v-if="user">Logged in as: {{ user.name }}</p>
-    <SideBar />
-  </div>
-
-</Analytics>
+      
+      <!-- Login Form -->
+      <form class="form" v-show="showLoginForm" @click.stop @submit.prevent="submitAuth">
+        <div class="title">Welcome,<br><span>{{ isSignUp ? 'sign up to join us' : 'sign in to continue' }}</span></div>
+        <input type="email" placeholder="Email" v-model="email" class="input" />
+        <input type="password" placeholder="Password" v-model="password" class="input" />
+        <input v-if="isSignUp" type="text" placeholder="Full Name" v-model="fullName" class="input" />
+        <input v-if="isSignUp" type="password" placeholder="Confirm Password" v-model="confirmPassword" class="input" />
+        
+        <div v-if="authError" class="error-message">{{ authError }}</div>
+        
+        <div class="login-with">
+          <div class="button-log"><b>t</b></div>
+          <div class="button-log">
+            <svg class="icon" height="56.6934px" id="Layer_1" style="enable-background:new 0 0 56.6934 56.6934;" version="1.1" viewBox="0 0 56.6934 56.6934" width="56.6934px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <path d="M51.981,24.4812c-7.7173-0.0038-15.4346-0.0019-23.1518-0.001c0.001,3.2009-0.0038,6.4018,0.0019,9.6017  c4.4693-0.001,8.9386-0.0019,13.407,0c-0.5179,3.0673-2.3408,5.8723-4.9258,7.5991c-1.625,1.0926-3.492,1.8018-5.4168,2.139  c-1.9372,0.3306-3.9389,0.3729-5.8713-0.0183c-1.9651-0.3921-3.8409-1.2108-5.4773-2.3649  c-2.6166-1.8383-4.6135-4.5279-5.6388-7.5549c-1.0484-3.0788-1.0561-6.5046,0.0048-9.5805  c0.7361-2.1679,1.9613-4.1705,3.5708-5.8002c1.9853-2.0324,4.5664-3.4853,7.3473-4.0811c2.3812-0.5083,4.8921-0.4113,7.2234,0.294  c1.9815,0.6016,3.8082,1.6874,5.3044,3.1163c1.5125-1.5039,3.0173-3.0164,4.527-4.5231c0.7918-0.811,1.624-1.5865,2.3908-2.4196  c-2.2928-2.1218-4.9805-3.8274-7.9172-4.9056C32.0723,4.0363,26.1097,3.995,20.7871,5.8372  C14.7889,7.8907,9.6815,12.3763,6.8497,18.0459c-0.9859,1.9536-1.7057,4.0388-2.1381,6.1836  C3.6238,29.5732,4.382,35.2707,6.8468,40.1378c1.6019,3.1768,3.8985,6.001,6.6843,8.215c2.6282,2.0958,5.6916,3.6439,8.9396,4.5078  c4.0984,1.0993,8.461,1.0743,12.5864,0.1355c3.7284-0.8581,7.256-2.6397,10.0725-5.24c2.977-2.7358,5.1006-6.3403,6.2249-10.2138  C52.5807,33.3171,52.7498,28.8064,51.981,24.4812z"></path>
+            </svg>
+          </div>
+          <div class="button-log">
+            <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="56.693px" viewBox="0 0 56.693 56.693" version="1.1" id="Layer_1" height="56.693px" class="icon">
+              <path d="M40.43,21.739h-7.645v-5.014c0-1.883,1.248-2.322,2.127-2.322c0.877,0,5.395,0,5.395,0V6.125l-7.43-0.029  c-8.248,0-10.125,6.174-10.125,10.125v5.518h-4.77v8.53h4.77c0,10.947,0,24.137,0,24.137h10.033c0,0,0-13.32,0-24.137h6.77  L40.43,21.739z"></path>
+            </svg>
+          </div>
+        </div>
+        <button class="button-confirm" type="submit">{{ isSignUp ? 'Sign Up →' : 'Let`s go →' }}</button>
+      </form>
+    </div>
+  </Analytics>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue';
-import { appwriteService } from '@/config/appwrite';
-
-const user = ref(null);
-
-const checkSession = async () => {
-  try {
-    const session = await appwriteService.account.get();
-    user.value = session;
-  } catch (error) {
-    console.error('No active session:', error);
-  }
-};
-
-onMounted(() => {
-  checkSession();
-});
-</script>
-
 <script>
-import { inject } from "@vercel/analytics"
-import Sidebar from '@/components/SideBar.vue'
-import { ref, onMounted } from 'vue';
-import { appwriteService } from '@/config/appwrite';
+import { inject } from "@vercel/analytics";
+import Sidebar from '@/components/SideBar.vue';
+import { ref, onMounted, computed } from 'vue';
+import { appwriteService, account } from '@/config/appwrite';
 
 export default {
   components: {
@@ -109,9 +87,21 @@ export default {
       email: '',
       password: '',
       confirmPassword: '',
-      isLoggedIn: false,
-      username: 'Guest User',
-      userPlan: 'Free Plan'
+      fullName: '',
+      currentUser: null,
+      authError: '',
+      isLoading: false
+    }
+  },
+  computed: {
+    isLoggedIn() {
+      return !!this.currentUser;
+    },
+    username() {
+      return this.currentUser ? this.currentUser.name : 'Guest User';
+    },
+    userPlan() {
+      return this.currentUser ? 'Premium Plan' : 'Free Plan';
     }
   },
   methods: {
@@ -125,38 +115,112 @@ export default {
       this.email = '';
       this.password = '';
       this.confirmPassword = '';
+      this.fullName = '';
+      this.authError = '';
     },
     showAuthForm(type) {
       this.isSignUp = type === 'signup';
       this.showLoginForm = true;
       this.showOverlay = true;
     },
-    submitAuth() {
-      if (this.isSignUp && this.password !== this.confirmPassword) {
-        alert('Passwords do not match!');
-        return;
+    async submitAuth() {
+      this.authError = '';
+      this.isLoading = true;
+      
+      try {
+        if (this.isSignUp) {
+          if (this.password !== this.confirmPassword) {
+            this.authError = 'Passwords do not match!';
+            return;
+          }
+          
+          // Create new user account
+          const userData = await appwriteService.createEmailAccount(
+            this.email,
+            this.password,
+            this.fullName || this.email.split('@')[0]
+          );
+          
+          this.currentUser = userData;
+          
+          // Send welcome email (optional)
+          try {
+            await appwriteService.sendWelcomeEmail(this.email, this.fullName);
+          } catch (err) {
+            console.warn('Welcome email could not be sent:', err);
+          }
+        } else {
+          // Login existing user
+          const userData = await appwriteService.login(this.email, this.password);
+          this.currentUser = userData;
+        }
+        
+        // Close the form on success
+        this.closeModals();
+        
+      } catch (error) {
+        console.error('Authentication error:', error);
+        this.authError = error.message || 'Authentication failed. Please try again.';
+      } finally {
+        this.isLoading = false;
       }
-      
-      // For demo purposes, simulate successful authentication
-      this.isLoggedIn = true;
-      this.username = this.email.split('@')[0];
-      this.userPlan = 'Premium Plan';
-      
-      // Close the form
-      this.closeModals();
+    },
+    async handleLogout() {
+      try {
+        await appwriteService.logout();
+        this.currentUser = null;
+      } catch (error) {
+        console.error('Logout failed:', error);
+      }
+    },
+    async checkSession() {
+      try {
+        const session = await account.get();
+        if (session.$id) {
+          // Get full user data from our custom users collection
+          const userData = await this.getUserData(session.$id);
+          this.currentUser = { ...session, ...userData };
+        }
+      } catch (error) {
+        console.log('No active session');
+      }
+    },
+    async getUserData(appwriteUserId) {
+      try {
+        const credentials = await appwriteService.databases.listDocuments(
+          appwriteService.DATABASE_ID,
+          appwriteService.COLLECTIONS.AUTH_CREDENTIALS,
+          [
+            appwriteService.databases.Query.equal('provider_user_id', appwriteUserId),
+            appwriteService.databases.Query.equal('provider', 'email')
+          ]
+        );
+        
+        if (credentials.total > 0) {
+          const userId = credentials.documents[0].user_id;
+          
+          const users = await appwriteService.databases.listDocuments(
+            appwriteService.DATABASE_ID,
+            appwriteService.COLLECTIONS.USERS,
+            [appwriteService.databases.Query.equal('user_id', userId)]
+          );
+          
+          if (users.total > 0) {
+            return users.documents[0];
+          }
+        }
+        
+        return null;
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        return null;
+      }
     }
-
-    
-
-
-
+  },
+  mounted() {
+    this.checkSession();
   }
-
-
-  
-
 }
-
 </script>
 
 <style scoped>
@@ -357,6 +421,13 @@ body {
   font-weight: 600;
   color: var(--font-color);
   cursor: pointer;
+}
+
+.error-message {
+  color: #e46363;
+  font-size: 12px;
+  text-align: center;
+  width: 100%;
 }
 
 /* Responsive adjustments */
